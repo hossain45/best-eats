ef05796ac07d99bf01badc608f95d8f90e7c4510
hossain45 <hossain45@student.sust.edu>
2023-09-14 15:03:38 +0600

    headline cards updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
@@ -5,6 +7,8 @@ function App() {
   return (
     <>
     <Navbar />
+    <Hero />
+    <HeadlineCards />
     </>
   )
 }
