07fff5e6573c82d8fe0f7a3fd274266e1d3c1377
hossain45 <hossain45@student.sust.edu>
2023-09-16 14:39:33 +0600

    cart drawer added

---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react"
+
+const Cart = () => {
+  
+  return (
+    <div>
+      <li className='text-xl py-4 flex cursor-pointer hover:scale-110 hover:bg-black/5  '>
+         Invite Friends
+      </li>
+    </div>
+  )
+}
+
+export default Cart
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,13 @@ import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs'
 import { TbTruckDelivery } from 'react-icons/tb'
 import { MdFavorite, MdHelp } from 'react-icons/md'
 import { FaUserFriends, FaWallet } from 'react-icons/fa'
+import Cart from './Cart'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
+  const [section, setSection] = useState(false)
+  
+
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
       {/* left side */}
@@ -29,7 +33,7 @@ const Navbar = () => {
         <input type="text" placeholder='Search food...' className='bg-transparent p-2 focus:outline-none w-full'/>
       </div>
       {/* cart btn */}
-      <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>  
+      <button onClick={() => setSection(!section)} className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>  
         <BsFillCartFill size={20} className='mr-2'/> Cart
       </button>
 
@@ -37,7 +41,7 @@ const Navbar = () => {
         {/* overlay */}
         {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
         
-        {/* side drawer menu */}
+        {/* left side drawer menu */}
         <div  className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white/90 z-10 duration-300' 
         : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white/90 z-10 duration-300'}>
           <AiOutlineClose 
@@ -73,7 +77,31 @@ const Navbar = () => {
               </li>
             </ul>
           </nav>
-        </div>      
+        </div> 
+
+      {/* cart section */}
+        {/* overlay */}
+        {section ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 right-0'></div> : ''}
+        
+        {/* cart drawer menu */}
+        <div  className={section ? 'fixed top-0 right-0 w-[300px] h-screen bg-white/90 z-10 duration-300' 
+        : 'fixed top-0 right-[-100%] w-[300px] h-screen bg-white/90 z-10 duration-300'}>
+          <AiOutlineClose 
+            size={30} 
+            className='absolute top-4 right-4 cursor-pointer hover:scale-110 hover:bg-black/5'
+            onClick={() => setSection(!section)}  
+          />
+          <h2 className='text-2xl p-4 '>
+            Your Cart 
+          </h2>
+          <div>
+            <ul className='flex flex-col p-4 text-gray-900'>
+                                     
+              <Cart />
+            </ul>
+          </div>
+        </div>
+           
     </div>
   )
 }
