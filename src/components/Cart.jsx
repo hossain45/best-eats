8773abf28adda585090582b4a9215de46e48961b
hossain45 <hossain45@student.sust.edu>
2023-09-20 15:18:06 +0600

    updated prev commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,17 +15,17 @@ const Cart = ({ cart, handleCountPlus, handleCountMinus, count, price, totalPric
             <div className="flex gap-3 items-center">
               <img className="h-[70px] w-[70px]" src={cartItem.image} alt={cartItem.name} />
               <div>
-                <h2 className="text-xl font-bold">{cartItem.name}</h2>
                 {/* same result can be done by both ternary operaor and useEffect */}
                 {/* {cartItem.price * (count[cartItem.id] !== undefined ? count[cartItem.id] : 1)} */}
                 <div className="flex justify-center items-center">
-                  <p className="font-semibold item-price">Price: $ {price[cartItem.id]} </p>
-                  <button onClick={() => handleDelete(cartItem.id)} className=" hover:bg-slate-100 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 30 30"  className="block m-0 p-0">
+                  <h2 className="text-xl font-bold">{cartItem.name}</h2>
+                  <button onClick={() => handleDelete(cartItem.id)} className=" hover:bg-slate-100 border-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 30 30"  className="block m-0 p-0">
                       <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
                     </svg>
                   </button>
                 </div>
+                <p className="font-semibold item-price">Price: $ {price[cartItem.id]} </p>
               </div>
             </div>
             <div className="text-center ">
