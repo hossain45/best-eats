be040af9427d8d9b38a7ba7e01f640eba9f16e24
hossain45 <hossain45@student.sust.edu>
2023-09-23 12:22:56 +0600

    working on single data

---
+++ src/components/Food.jsx
@@ -0,0 +1,14 @@
+
+
+const Food = () => {
+  return (
+    <>
+      <div className=" flex-col sm:flex justify-center items-center ">
+        <img src="https://unsplash.com/photos/CK6tjAIMJWM" alt="" />
+      </div>
+      <div></div>
+    </>
+  )
+}
+
+export default Food
